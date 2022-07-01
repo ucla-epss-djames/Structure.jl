6a28c2635b92b2285da55018b80eeaad054a5da6
David James <davidabraham@ucla.edu>
2022-07-01 02:06:08 -0700

    got rid of some dependancies and refactoring how libraries work with each other. m is now gm where physical constant is taken care of outside of library operations

--- src/Structure.jl
+++ src/Structure.jl
@@ -1,25 +1,22 @@
 module Structure
 
 using SpecialFunctions: gamma
-using PhysicalConstants.CODATA2014: G
-using QuadGK: quadgk
 
 export dmdr, dPdr
 export planet_m, planet_g, planet_mmotion, planet_mu, planet_eta, planet_cmu, planet_structure
 
 # Governing Equations
 dmdr(r::Real, ρ::Real) = 4π * ρ * r^2
-dPdr(r::Real, ρ::Real, m::Real) = -G.val * m * ρ / r^2
+dPdr(r::Real, ρ::Real, gm::Real) = -gm * ρ / r^2
 dPdr(ρ::Real, g::Real) = -ρ * g
 
 # Planetary Structure EQNs
 planet_m(r::Real, ρ::Real) = 4π * ρ * r^2
-planet_g(r::Real, m::Real) = r == 0 ? 0 : G.val * m / r^2
-planet_mmotion(m::Real, a::Real) = sqrt(G.val * m / a^3)
+planet_g(r::Real, gm::Real) = r == 0 ? 0 : gm / r^2
+planet_mmotion(gm::Real, a::Real) = sqrt(gm / a^3)
 planet_mu(r::Real, g::Real, ρ::Real) = ρ * g * r
 planet_mu(T::Real, P::Real) = (101 + P*1e-9/2.41 - (T-1650)/23.4) * 1e9
 
-planet_eta(η0::Real, A::Real, T_m::Real, T::Real) = η0 * exp(A * (T_m/T - 1))
 
 # Rheaology Models
 function cmu_maxwell(μ::Real, ω::Real, η::Real)
@@ -90,41 +87,6 @@ function planet_cmu(μ::Real, ω::Real, η::Real, r::Real, g::Real,
     end
 
     return cmu
-
-end
-
-function planet_structure(plnt, data::Matrix)
-
-    mass = 0.0
-    r0 = 0.0
-    layers = plnt.layers
-    ω = plnt.ω
-    μ_f = plnt.μ_f
-    model = plnt.rhea_model
-
-    sd = zeros(Complex, layers, 4)
-
-    for i in 1:layers
-
-        r1 = data[i,1]
-        ρ = data[i,2]
-        μ = data[i,3]
-        η = data[i,4]
-
-        if(i != 1) r0 = real(sd[i-1,1]) end
-        res, err = quadgk(x -> planet_m(x, ρ), r0, r1)
-        mass += res
-
-        g = planet_g(r1, mass)
-
-        sd[i,4] = ρ
-        sd[i,3] = g
-        sd[i,2] = planet_cmu(μ, ω, η, r1, g, ρ, μ_f, model)
-        sd[i,1] = r1
-
-    end
-
-    return sd, mass
 
 end
 
